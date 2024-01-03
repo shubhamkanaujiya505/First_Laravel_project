895117fa179cfe6d5717825f17dd85a13f3b27b1
shubham kanaujiya <shubham.kanaujiya@gingerwebs.co.in>
2024-01-03 22:02:32 +0530

    route single parameter passing

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', function () {
     return view('home   ');
 });
-Route::get('/about', function () {
-    return view('about');
+Route::get('/about/{id?}', function (string $id=null) {
+    if($id !=null){
+    return "<h1> post id:".$id."</h1>";
+    }else{
+    return "<h1> No id found </h1>";
+    }
 });
 // Route::view('about', '/about');
 
