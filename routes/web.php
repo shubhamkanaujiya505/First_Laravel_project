ed06dad2c0c1f123aee72f8dd5d39476a6370d48
shubham kanaujiya <shubham.kanaujiya@gingerwebs.co.in>
2024-01-03 22:29:49 +0530

    route multiple parameter constraints with input validation

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', function () {
     return view('home   ');
 });
-Route::get('/about/{id?}/comment/{comment?}', function (string $id=null, string $comment=null) {
+Route::get('/about/{id}/comment/{commentid}', function (string $id, string $comment) {
     if($id !=null){
-    return "<h1> post id:".$id."</h1><h1> post comment:".$comment."</h1>";
+    return "<h1> post id:".$id."</h1><h1> post id:".$comment."</h1>";
     }else{
     return "<h1> No id found </h1>";
     }
-});
+})->whereNumber('id')->whereAlpha('$commentid');
+
+// Laravel Route Constraints
+// http://localhost/post/10 ----> whereNumber('id');
+// http://localhost/post/hai ----> whereAlpha('Name');
+// http://localhost/post/hia10 ----> whereAlphaNumeric('name');
+// http://localhost/post/song ----> whereIn('category',['movie','song'])
+// http://localhost/post/@10 ----> where('id','[@0-9]+'); regular expression
+
+
+
 // Route::view('about', '/about');
 
 Route::get('/group', function () {
